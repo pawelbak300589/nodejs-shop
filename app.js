01c6d69469fe96c32a20e608941c38311b97d34a
Pawel Bak <pawelbak300589@gmail.com>
2020-07-29 13:52:47 +0100

    Create main layout with Ejs templating engine & Add Ejs views files

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const path = require('path');
 
 const express = require('express');
-const expressHbs = require('express-handlebars');
 
 const rootDir = require('./util/path');
 
 const app = express();
 
-app.engine('handlebars', expressHbs({ layoutsDir: 'views/layouts/', defaultLayout: 'main-layout' }));
-app.set('view engine', 'handlebars');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
