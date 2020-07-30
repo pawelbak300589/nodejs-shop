8ee3b0f64c3d89d2d8fdcbfe135ad16ecdb10768
Pawel Bak <pawelbak300589@gmail.com>
2020-07-30 10:14:55 +0100

    Add more details to the product, Update controller, model and view

--- models/product.js
+++ models/product.js
@@ -14,9 +14,11 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-    constructor(title) {
+    constructor(title, imageUrl, description, price) {
         this.title = title;
-        this.filePath = path.join(dirName, 'data', 'products.json');
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
 
     save() {
