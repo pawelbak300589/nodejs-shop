c56f9c2647f74df72e6806ff44eab69f2a963a98
Pawel Bak <pawelbak300589@gmail.com>
2020-07-31 14:00:34 +0100

    Add user model, Add creating new user on server start and loading it to request

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -16,13 +18,38 @@ const shopRoutes = require('./routes/shop');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync()
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+
+sequelize
+    // .sync({ force: true })
+    .sync()
     .then(result => {
+        return User.findByPk(1);
+    })
+    .then(user => {
+        if (!user) {
+            return User.create({
+                name: 'Pawel',
+                email: 'info@pawelbak.me'
+            });
+        }
+        return user;
+    })
+    .then(user => {
         app.listen(4000, () => {
             console.log('Listening on 4000');
         });
