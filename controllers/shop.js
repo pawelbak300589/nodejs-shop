161cbce2d75d12a0cecfab151a29f722f149a0cb
Pawel Bak <pawelbak300589@gmail.com>
2020-07-31 15:33:32 +0100

    Create order and orderItem model, Add postOrder route and controller method

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,4 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
 
 exports.getProducts = (req, res) => {
     Product.findAll()
@@ -107,6 +106,28 @@ exports.postCartDeleteProduct = (req, res) => {
         })
         .then(result => {
             res.redirect('/cart');
+        })
+        .catch(err => console.log(err));
+};
+
+exports.postOrder = (req, res) => {
+    req.user.getCart()
+        .then(cart => {
+            return cart.getProducts();
+        })
+        .then(products => {
+            return req.user.createOrder()
+                .then(order => {
+                    return order.addProducts(products.map(product => {
+                            product.orderItem = { quantity: product.cartItem.quantity };
+                            return product;
+                        })
+                    );
+                })
+                .catch(err => console.log(err));
+        })
+        .then(result => {
+            res.redirect('/orders');
         })
         .catch(err => console.log(err));
 };
