f10d78fbc55ab5fdcc35ca73b95fcd9c02052a62
Pawel Bak <pawelbak300589@gmail.com>
2020-08-03 16:02:53 +0100

    Add get and add orders methods to user model, Update shop controller to use new methods

--- controllers/shop.js
+++ controllers/shop.js
@@ -72,26 +72,7 @@ exports.postCartDeleteProduct = (req, res) => {
 };
 
 exports.postOrder = (req, res) => {
-    let fetchedCart;
-    req.user.getCart()
-        .then(cart => {
-            fetchedCart = cart;
-            return cart.getProducts();
-        })
-        .then(products => {
-            return req.user.createOrder()
-                .then(order => {
-                    return order.addProducts(products.map(product => {
-                            product.orderItem = { quantity: product.cartItem.quantity };
-                            return product;
-                        })
-                    );
-                })
-                .catch(err => console.log(err));
-        })
-        .then(result => {
-            return fetchedCart.setProducts(null);
-        })
+    req.user.addOrder()
         .then(result => {
             res.redirect('/orders');
         })
--- models/user.js
+++ models/user.js
@@ -60,6 +60,41 @@ class User {
         );
     }
 
+    addOrder() {
+        const db = getDb();
+        return this.getCart()
+            .then(products => {
+                const order = {
+                    items: products,
+                    user: {
+                        _id: new mongodb.ObjectId(this._id),
+                        name: this.name
+                    }
+                };
+                return db.collection('orders').insertOne(order);
+            })
+            .then(result => {
+                this.cart = { items: [] };
+                return db.collection('users')
+                    .updateOne(
+                        { _id: new mongodb.ObjectId(this._id) },
+                        { $set: { cart: { items: [] } } }
+                    );
+            })
+            .catch(err => console.log(err));
+    }
+
+    getOrders() {
+        const db = getDb();
+        return db.collection('orders')
+            .find({ _id: new mongodb.ObjectId(userId) });
+        // .then(user => {
+        //     console.log(user);
+        //     return user;
+        // })
+        // .catch(err => console.log(err));
+    }
+
     static findById(userId) {
         const db = getDb();
         return db.collection('users')
