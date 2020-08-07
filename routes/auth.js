e1d6faf358184e60842001a66ea08bcbd2ccd350
Pawel Bak <pawelbak300589@gmail.com>
2020-08-07 11:02:06 +0100

    Add data sanitization for signup inputs to remove whitespaces and to normalize email addresses

--- routes/auth.js
+++ routes/auth.js
@@ -14,10 +14,12 @@ router.post('/login',
     [
         body('email')
             .isEmail()
-            .withMessage('Please enter a valid email address.'),
+            .withMessage('Please enter a valid email address.')
+            .normalizeEmail(),
         body('password', 'Password has to be valid.')
             .isLength({ min: 5 })
             .isAlphanumeric()
+            .trim()
     ],
     authController.postLogin
 );
@@ -34,11 +36,14 @@ router.post('/signup',
                             return Promise.reject('E-mail exists already, please pick a different one.');
                         }
                     });
-            }),
+            })
+            .normalizeEmail(),
         body('password', 'Please enter a password with only numbers and text and at least 5 characters.')
             .isLength({ min: 5 })
-            .isAlphanumeric(),
+            .isAlphanumeric()
+            .trim(),
         body('confirmPassword')
+            .trim()
             .custom((value, { req }) => {
                 if (value !== req.body.password) {
                     throw new Error('Passwords have to match!');
